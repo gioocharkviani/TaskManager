bde06903df695984f4ca0aa29a9eeeb4b90a5bd1
gioocharkviani <gioocharkviani@gmail.com>
2023-05-12 11:43:51 +0400

    dsaf34324

--- src/components/content/Dnd/DndTasks.js
+++ src/components/content/Dnd/DndTasks.js
@@ -7,46 +7,57 @@ import TaskCard from './TaskCard'
 
 
 
-const onDragEnd = (result, columns, setColumns) => {
-  if (!result.destination) return;
-  const { source, destination } = result;
-
-  if (source.droppableId !== destination.droppableId) {
-    const sourceColumn = columns[source.droppableId];
-    const destColumn = columns[destination.droppableId];
-    const sourceItems = [...sourceColumn.items];
-    const destItems = [...destColumn.items];
-    const [removed] = sourceItems.splice(source.index, 1);
-    destItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...sourceColumn,
-        items: sourceItems
-      },
-      [destination.droppableId]: {
-        ...destColumn,
-        items: destItems
-      }
-    });
-  } else {
-    const column = columns[source.droppableId];
-    const copiedItems = [...column.items];
-    const [removed] = copiedItems.splice(source.index, 1);
-    copiedItems.splice(destination.index, 0, removed);
-    setColumns({
-      ...columns,
-      [source.droppableId]: {
-        ...column,
-        items: copiedItems
-      }
-    });
-  }
-};
-
 function DndTasks() {
+
   const [columns, setColumns] = useState(columnsFromBackend);
-  console.log(columns);
+
+
+
+//Search Filter  
+  const [searchQuery, setSearchQuery] = useState("first");
+  const filteredItems = columnsFromBackend[Object.keys(columnsFromBackend)[0]].items.filter(
+    (item) => item.content.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+  
+//Search Filter
+
+  const onDragEnd = (result, columns, setColumns) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+  
+    if (source.droppableId !== destination.droppableId) {
+      const sourceColumn = columns[source.droppableId];
+      const destColumn = columns[destination.droppableId];
+      const sourceItems = [...sourceColumn.items];
+      const destItems = [...destColumn.items];
+      const [removed] = sourceItems.splice(source.index, 1);
+      destItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...sourceColumn,
+          items: sourceItems
+        },
+        [destination.droppableId]: {
+          ...destColumn,
+          items: destItems
+        }
+      });
+    } else {
+      const column = columns[source.droppableId];
+      const copiedItems = [...column.items];
+      const [removed] = copiedItems.splice(source.index, 1);
+      copiedItems.splice(destination.index, 0, removed);
+      setColumns({
+        ...columns,
+        [source.droppableId]: {
+          ...column,
+          items: copiedItems
+        }
+      });
+    }
+  };
+
   return (
     <div className="flex  gap-[20px] w-full  overflow-x-auto" >
       <DragDropContext
@@ -66,15 +77,20 @@ function DndTasks() {
                         style={{background: snapshot.isDraggingOver ? "#9ab6e8": "#d8e3f6" }}
                         className="flex flex-col min-w-[250px]  min-h-[100px]"
                       >
+                        
                         {column.items.map((item, index) => {
                           return (
                             <Draggable key={item.id} draggableId={item.id} index={index} >
                               {(provided, snapshot) => {
+                      
                                 return (
+                                
                                   <div className="mb-[10px]" ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps} >
                                     <TaskCard desc={item.content.descirption} title={item.content.title}/>
                                   </div>
+
                                 );
+
                               }}
                             </Draggable>
                           );
--- src/components/content/Dnd/TaskSearchBar.js
+++ src/components/content/Dnd/TaskSearchBar.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import Input from '../../UI/Input'
+import React, { useState } from 'react'
 import Search from '../../../assets/search.svg'
 
 const TaskSearchBar = () => {
+const [searchValue , setSearchalue] = useState('')
+const SearchTask = (e) =>{
+  setSearchalue(e.target.value);
+}
+
   return (
+
     <div>
                 <form className='rounded-[3px] flex gap-[5px] py-[5px] px-[10px] border-[1px] border-[gray]'>
-                    <input type='text' placeholder='Search Task'  className='outline-none text-[13px]'/>
+                    <input onChange={SearchTask} type='text' placeholder='Search Task'  className='outline-none text-[13px]'/>
                     <button><img src={Search} /></button>
                 </form>
     </div>
